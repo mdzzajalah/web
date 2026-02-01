b0ab19e49a2eba161a75994facaf663ee0129b41
mdzzajalah <bel4jar1d@gmail.com>
2026-02-01 11:40:06 +0700

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,21 @@
+const inputField = document.getElementById('email');
+    const actionBtn = document.getElementById('lanjutkanBtn');
+
+    actionBtn.addEventListener('click', function() {
+        // Validasi: Cek apakah input kosong
+        if (inputField.value.trim() === "") {
+            inputField.classList.add('error-border');
+            console.log("Input kosong, perintah ditolak!");
+        } else {
+            inputField.classList.remove('error-border');
+            // Kalau isi ada, arahkan ke halaman baru (Hyperlink)
+            window.location.href = "login.html", "_blank"; 
+        }
+    });
+
+    // Hapus border merah pas user mulai ngetik lagi
+    inputField.addEventListener('input', function() {
+        if (this.value !== "") {
+            this.classList.remove('error-border');
+        }
+    });
